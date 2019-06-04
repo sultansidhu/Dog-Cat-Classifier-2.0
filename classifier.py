260458cdb5361ca6104ed8f1bfff50f01fe89170
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-06-04 11:25:19 -0400

    preliminary model established. accuracy at ~72%

--- classifier.py
+++ classifier.py
@@ -8,31 +8,52 @@ from keras import layers
 from keras import models
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
 
 # MAKING DIRECTORIES AND MOVING FILES
 original_dataset = '/Users/sultansidhu/Desktop/development/python/ai/classifier(dogs vs cats)/dogs-vs-cats'
 directories = []
 
-dogs_train_set = os.path.join(original_dataset, 'dogs_train_set')
-os.mkdir(dogs_train_set)
+# making sub directories for training, validation and testing
+master_train_path = os.path.join(original_dataset, 'master_train')
+master_validation_path = os.path.join(original_dataset, 'master_validation')
+master_test_path = os.path.join(original_dataset, 'master_test')
+try:
+    os.mkdir(master_train_path)
+    os.mkdir(master_validation_path)
+    os.mkdir(master_test_path)
+except FileExistsError:
+    print("File exists, moving on...")
+
+# making the dog directories
+dogs_train_set = os.path.join(master_train_path, 'dogs_train_set')
 directories.append(dogs_train_set)
-dogs_validation_set = os.path.join(original_dataset, 'dogs_validation_set')
-os.mkdir(dogs_validation_set)
+dogs_validation_set = os.path.join(master_validation_path, 'dogs_validation_set')
 directories.append(dogs_validation_set)
-dogs_test_set = os.path.join(original_dataset, 'dogs_test_set')
-os.mkdir(dogs_test_set)
+dogs_test_set = os.path.join(master_test_path, 'dogs_test_set')
 directories.append(dogs_test_set)
+try:
+    os.mkdir(dogs_train_set)
+    os.mkdir(dogs_validation_set)
+    os.mkdir(dogs_test_set)
+except FileExistsError:
+    print("File exists, moving on...")
 
-cats_train_set = os.path.join(original_dataset, 'cats_train_set')
-os.mkdir(cats_train_set)
+# making the cat directories
+cats_train_set = os.path.join(master_train_path, 'cats_train_set')
 directories.append(cats_train_set)
-cats_validation_set = os.path.join(original_dataset, 'cats_validation_set')
-os.mkdir(cats_validation_set)
+cats_validation_set = os.path.join(master_validation_path, 'cats_validation_set')
 directories.append(cats_validation_set)
-cats_test_set = os.path.join(original_dataset, 'cats_test_set')
-os.mkdir(cats_test_set)
+cats_test_set = os.path.join(master_test_path, 'cats_test_set')
 directories.append(cats_test_set)
+try:
+    os.mkdir(cats_train_set)
+    os.mkdir(cats_validation_set)
+    os.mkdir(cats_test_set)
+except FileExistsError:
+    print("File exists, moving on...")
 
+# file transfer
 train_src = os.path.join(original_dataset, 'train')
 
 filenames = ['cat.{}.jpg'.format(i) for i in range(0, 4000)]
@@ -92,17 +113,37 @@ model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4),
 
 # DATA PREPROCESSING
 
-dogs_train_datagen = ImageDataGenerator(rescale=1./255)
-cats_train_datagen = ImageDataGenerator(rescale=1./255)
-dogs_test_datagen = ImageDataGenerator(rescale=1./255)
-cats_test_datagen = ImageDataGenerator(rescale=1./255)
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
 
-cats_train_generator = cats_train_datagen.flow_from_directory(cats_train_set, target_size=(150, 150),
-                                                             batch_size=20, class_mode='binary')
-dogs_train_generator = dogs_train_datagen.flow_from_directory(dogs_train_set, target_size=(150, 150),
-                                                             batch_size=20, class_mode='binary')
-cats_validation_generator = cats_test_datagen.flow_from_directory(cats_validation_set, target_size=(150, 150),
-                                                             batch_size=20, class_mode='binary')
-dogs_validation_generator = dogs_test_datagen.flow_from_directory(dogs_validation_set, target_size=(150, 150),
-                                                             batch_size=20, class_mode='binary')
+train_generator = train_datagen.flow_from_directory(master_train_path, target_size=(150, 150),
+                                                    batch_size=20, class_mode='binary')
+validation_generator = test_datagen.flow_from_directory(master_validation_path, target_size=(150, 150),
+                                                  batch_size=20, class_mode='binary')
+history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=30,
+                              validation_data=validation_generator, validation_steps=50)
 
+if __name__ == "__main__":
+
+    # PLOTTING GRAPH DYNAMICS
+
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, 1 + len(acc))
+
+    plt.plot(epochs, acc, 'bo', label='Training Acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation Acc')
+    plt.title('Training and Validation Accuracy')
+    plt.legend()
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'bo', label='Training Loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+    plt.title('Training and Validation Loss')
+    plt.legend()
+
+    plt.show()
